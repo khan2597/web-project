6cee49cd37568607de150f053e03efd8acdede14
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-10-30 10:33:01 +0000

    added test to enter team names

---
+++ spec/features/enter_team_names_spec.rb
@@ -0,0 +1,9 @@
+feature 'Enter names' do
+    scenario 'submit team names' do
+        visit('/')
+        fill_in :home_team, with: 'Los Angeles Lakers'
+        fill_in :away_team, with: 'Los Angeles Clippers'
+        click_button 'Submit'
+        expect(page).to have_content 'Los Angeles Lakers vs. Los Angeles Clippers'
+    end
+end
